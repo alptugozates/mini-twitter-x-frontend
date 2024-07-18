1319d6ed5e07bd1b7c956adaac3bc8bc47677f0d
alptugozates <omeralptugozates@gmail.com>
2024-07-18 20:53:13 +0300

    projeyi kurar

---
+++ mini-twitter/src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./Navbar";
+
+function App() {
+  return (
+    // <Router>
+    <div>
+      <Navbar />
+    </div>
+    /* </Router> */
+  );
+}
+
+export default App;
