679d00e324083582df33ecba07d6f40b10702097
alptugozates <omeralptugozates@gmail.com>
2024-07-18 22:05:04 +0300

    router fixler

--- mini-twitter/src/App.js
+++ mini-twitter/src/App.js
@@ -1,16 +1,22 @@
 import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Navbar from "./Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import LoginForm from "./components/LoginForm";
 
 function App() {
   return (
-    // <Router>
-    <div>
-      <Navbar />
-    </div>
-    /* </Router> */
+    <Router>
+      <div>
+        <Routes>
+          <Route exact path="/" element={<Navbar />} />
+        </Routes>
+        <Routes>
+          <Route exact path="/login" element={<LoginForm />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
