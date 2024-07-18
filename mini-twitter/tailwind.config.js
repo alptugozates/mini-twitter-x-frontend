21393bda8fe4f7b57dfba276a74ef881a99ae785
alptugozates <omeralptugozates@gmail.com>
2024-07-18 20:52:42 +0300

    proje, tailwindcss kurar

---
+++ mini-twitter/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+      colors: {
+        "custom-white": "#FFFFFF",
+        "custom-gray": "#737373",
+        "custom-bg-color": "rgba(45, 139, 192, 0.75)",
+        turquoise: "#23A6F0",
+        "turquoise-400": "#4FB8FF",
+      },
+    },
+  },
+  plugins: [],
+};
